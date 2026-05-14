39a082711b3de6937dcabc7bd196f1d75d8fbde8
Kirthika S <kirthika.suku07@gmail.com>
2026-05-14 21:20:17 +0530

    App.js Modified: Message is changed

--- app.js
+++ app.js
@@ -3,13 +3,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const name = "CodeTest";
 
-app.get("/", (req, res) => {
-    const message = `Welcome to ShopEasy from ${name}!`
-    res.send(message);
-})
-
-const server = app.listen(PORT, () => {
+app.get('/', (req, res) => {
+    res.send(`! Welcome to Shopeasy.`);
+});
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-})
-
-module.exports = {app,server,name };
+});
